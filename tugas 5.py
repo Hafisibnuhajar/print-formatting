9fc3cd45b608933a3155a681e58a22fd7cfe5530
Hafis Ibnu hajar <80692690+Hafisibnuhajar@users.noreply.github.com>
2021-04-05 14:29:39 +0700

    tugas 5
    teknik print formatting dalam python

---
+++ tugas 5.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+#strings print formating
+
+nama = "Hafis ibnu hajar"
+
+print(f'Nama Dosen pengampu mata kuliah animasi {nama}')
+
+
+# In[10]:
+
+
+nama = "Hafis ibnu hajar"
+f"Hallo {nama} selamat datang di Dehasen"
+
+
+# In[12]:
+
+
+user_nama = input ("siapa nama kamu? ")
+print(f'Hallo {user_nama} selamat datang di Dehasen')
+
+
+# In[ ]:
+
+
+
+
